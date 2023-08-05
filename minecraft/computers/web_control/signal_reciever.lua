3c2bef323f7f5b75e3e2b77bfb4afdcba7824e76
Yurtle (Lachlan Butler) <yurtle.butler@gmail.com>
2023-08-04 21:50:03 -0700

    more signal decoding

--- minecraft/computers/web_control/signal_reciever.lua
+++ minecraft/computers/web_control/signal_reciever.lua
@@ -1,3 +1,5 @@
+os.loadAPI("json")
+
 local params = {...}
 
 print(params)
@@ -8,4 +10,13 @@ if next(params) == nil then
     ws = http.websocket("wss://yurtle.net/cc/default")
 else
     ws = http.websocket("wss://yurtle.net/cc/" .. params[1])
+end
+
+while true do
+    local signal = json.decode(ws.receive())
+    if (signal.type == "signal") then
+        for key, value in pairs(signal.data.signal) do
+            print(value)
+        end
+    end
 end
