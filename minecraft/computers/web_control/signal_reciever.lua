12a15b93592666648bc7ef7a5d6983de82cdf85f
Yurtle (Lachlan Butler) <yurtle.butler@gmail.com>
2023-08-04 21:54:37 -0700

    added assert and made ws global

--- minecraft/computers/web_control/signal_reciever.lua
+++ minecraft/computers/web_control/signal_reciever.lua
@@ -4,16 +4,14 @@ local params = {...}
 
 print(params)
 
-local ws
-
 if next(params) == nil then
-    ws = http.websocket("wss://yurtle.net/cc/default")
+    WS = assert(http.websocket("wss://yurtle.net/cc/default"))
 else
-    ws = http.websocket("wss://yurtle.net/cc/" .. params[1])
+    WS = assert(http.websocket("wss://yurtle.net/cc/" .. params[1]))
 end
 
 while true do
-    local signal = json.decode(ws.receive())
+    local signal = json.decode(WS.receive())
     if (signal.type == "signal") then
         for key, value in pairs(signal.data.signal) do
             print(value)
