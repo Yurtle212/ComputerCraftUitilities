a4be8ffd854d1f0721db9b08c51bb36ccc44fbf7
Yurtle (Lachlan Butler) <yurtle.butler@gmail.com>
2023-08-04 19:34:28 -0700

    testing door

--- minecraft/computers/private_door/door.lua
+++ minecraft/computers/private_door/door.lua
@@ -1,1 +1,11 @@
-print("Door");
+local players = peripheral.find("playerDetector")
+
+while true do
+    os.startTimer(1)
+    os.pullEvent("timer")
+
+    local pos1 = {x=705, y=62, z=-1511}
+    local pos2 = {x=702, y=66, z=-1507}
+    local result = players.isPlayersInCoords(pos1, pos2)
+    print(result)
+end
