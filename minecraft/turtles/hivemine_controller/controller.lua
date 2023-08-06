850a149e92aca4ee3bd2254a6f7882c513c63aff
Yurtle (Lachlan Butler) <yurtle.butler@gmail.com>
2023-08-06 13:33:49 -0700

    added channel to controller printout

--- minecraft/turtles/hivemine_controller/controller.lua
+++ minecraft/turtles/hivemine_controller/controller.lua
@@ -4,4 +4,4 @@ if (params[1] ~= nil) then
     rednet.CHANNEL_BROADCAST = params[1]
 end
 
-print("Controller starting...")
+print("Controller starting on channel" .. rednet.CHANNEL_BROADCAST .. "...")
