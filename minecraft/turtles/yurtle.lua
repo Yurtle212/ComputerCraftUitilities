cf6e879d186a13793b32a2bbe08cd23e709be6ef
Yurtle (Lachlan Butler) <yurtle.butler@gmail.com>
2023-08-07 18:57:45 -0700

    moved findItemInInventory ahead of refuel

--- minecraft/turtles/yurtle.lua
+++ minecraft/turtles/yurtle.lua
@@ -6,32 +6,6 @@ local fuelItems = {
 
 local NUM_SLOTS = 16
 
-local function refuel(amount)
-    if (amount == nil) then
-        amount = turtle.getFuelLimit() - turtle.getFuelLevel()
-    end
-    local fueledAmount = 0
-
-    while fueledAmount < amount do
-        local slot = yurtle.findItemInInventory("fuel")
-        if (slot == nil) then
-            break
-        end
-
-        turtle.select(slot)
-        local detail = turtle.getItemDetail(slot)
-        if (fuelItems[detail.name] * detail.count) > amount then
-            while fueledAmount < amount do
-                turtle.refuel(1)
-                fueledAmount = fueledAmount + fuelItems[detail.name] * detail.count
-            end
-        else
-            turtle.refuel()
-            fueledAmount = fueledAmount + fuelItems[detail.name] * detail.count
-        end
-    end
-end
-
 local function findItemInInventory(itemName)
     for i = 1, NUM_SLOTS, 1 do
         local item = turtle.getItemDetail(i)
@@ -50,6 +24,32 @@ local function findItemInInventory(itemName)
         end
     end
     return nil
+end
+
+local function refuel(amount)
+    if (amount == nil) then
+        amount = turtle.getFuelLimit() - turtle.getFuelLevel()
+    end
+    local fueledAmount = 0
+
+    while fueledAmount < amount do
+        local slot = findItemInInventory("fuel")
+        if (slot == nil) then
+            break
+        end
+
+        turtle.select(slot)
+        local detail = turtle.getItemDetail(slot)
+        if (fuelItems[detail.name] * detail.count) > amount then
+            while fueledAmount < amount do
+                turtle.refuel(1)
+                fueledAmount = fueledAmount + fuelItems[detail.name] * detail.count
+            end
+        else
+            turtle.refuel()
+            fueledAmount = fueledAmount + fuelItems[detail.name] * detail.count
+        end
+    end
 end
 
 return { refuel = refuel, findItemInInventory = findItemInInventory, fuelItems = fuelItems, NUM_SLOTS = 16 }
